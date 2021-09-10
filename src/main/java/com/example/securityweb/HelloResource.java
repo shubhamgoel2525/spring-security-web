feba36fbea32faa7b236a9e729d00ceaf3ce3128
Shubham Goel <shubhamgoel2525@gmail.com>
2021-09-10 14:09:30 +0530

    Initial commit

---
+++ src/main/java/com/example/securityweb/HelloResource.java
@@ -0,0 +1,52 @@
+package com.example.securityweb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.securityweb.models.AuthenticationRequest;
+import com.example.securityweb.models.AuthenticationResponse;
+import com.example.securityweb.util.JwtUtil;
+
+@RestController
+public class HelloResource {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username and password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+}
---
+++ src/main/java/com/example/securityweb/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.securityweb.models;
+
+public class AuthenticationResponse {
+	private final String jwt;
+
+	public String getJwt() {
+		return jwt;
+	}
+
+	public AuthenticationResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+}
